98f9f71f306aec8e47a2dbb5eb2869e1c55990bb
kinghia <162483327+kinghia@users.noreply.github.com>
2024-07-23 15:53:25 +0700

    nghia update

--- Assets/Scripts/CarInputHandler.cs
+++ Assets/Scripts/CarInputHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CarInputHandler : MonoBehaviour
 {
+    public int playerNumber = 1;
     TopDownCarController topDownCarController;
 
     void Awake()
@@ -15,13 +16,27 @@ public class CarInputHandler : MonoBehaviour
     {
         Vector2 inputVector = Vector2.zero;
 
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
+        switch (playerNumber)
+        {
+            case 1:
+                inputVector.x = Input.GetAxis("Horizontal_P1");
+                inputVector.y = Input.GetAxis("Vertical_P1");
+                break;
+            case 2:
+                inputVector.x = Input.GetAxis("Horizontal_P2");
+                inputVector.y = Input.GetAxis("Vertical_P2");
+                break;
+            case 3:
+                inputVector.x = Input.GetAxis("Horizontal_P3");
+                inputVector.y = Input.GetAxis("Vertical_P3");
+                break;
+            case 4:
+                inputVector.x = Input.GetAxis("Horizontal_P4");
+                inputVector.y = Input.GetAxis("Vertical_P4");
+                break;
+        }
 
         topDownCarController.SetInputVector(inputVector);
-
-        if (Input.GetButtonDown("Jump"))
-            topDownCarController.Jump(1.0f, 0.0f);
 
     }
 }
